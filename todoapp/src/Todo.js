4d329339d016ab428e4761b3262f88dfc9b2cb9e
Breadn <bylueang@gmail.com>
2022-11-29 08:54:23 -0600

    todo setup

---
+++ todoapp/src/Todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Frosted Donuts To-Do App</h1>
+        <h3></h3>
+      </div>
+    );
+  }
+}
+
+export default App;
